7891a745735bf8d9dac30891809c07c789ebedbb
SJCRPV <sjcrpv.games@hotmail.com>
2016-04-17 17:14:05 +0100

    v

---
+++ Ludum-Dare-35/Assets/Scripts/PointToPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointToPlayer : MonoBehaviour {
+
+    private GameObject player;
+    private GameObject target;
+
+    void pointInRightDirection()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        target = GameObject.Find("Target");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pointInRightDirection();
+	}
+}
