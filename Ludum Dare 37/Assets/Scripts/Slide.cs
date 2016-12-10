2511896002dc3c8911a24b31c9a760f754118c29
SJCRPV <sjcrpv.games@hotmail.com>
2016-12-10 08:40:24 +0000

    End of Night 1 Pixel spawning and not much else

---
+++ Ludum Dare 37/Assets/Scripts/Slide.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+//FIX: Can't inherit from RigidbodyFirstPersonController
+public class Slide : RigidbodyFirstPersonController
+{
+    [SerializeField]
+    private float slideBoost;
+    [SerializeField]
+    private float slideDrag;
+
+    private CapsuleCollider playerCollider;
+    private Rigidbody playerRigidbody;
+
+    private IEnumerator slide()
+    {
+        playerCollider.height = playerCollider.height / 2;
+        playerRigidbody.drag = 0f;
+        playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, 0f, playerRigidbody.velocity.z);
+        playerRigidbody.AddForce(new Vector3(slideBoost, 0f, 0f), ForceMode.Impulse);
+
+        yield return new WaitForSeconds(1);
+
+        playerRigidbody.drag = slideDrag;
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        playerCollider = gameObject.GetComponent<CapsuleCollider>();
+        playerRigidbody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Slide"))
+        {
+            slide();
+        }
+	}
+}
---
+++ Ludum Dare 37/Assets/Scripts/TimedDestroy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedDestroy : MonoBehaviour {
+
+    [SerializeField]
+    private float timeBeforeDeath;
+
+    private float timeBeforeDeathStore;
+    private bool playerIsTouching;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        playerIsTouching = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        playerIsTouching = false;
+        timeBeforeDeath = timeBeforeDeathStore;
+    }
+
+    public bool isPlayerTouching()
+    {
+        return playerIsTouching;
+    }
+
+    private void Start()
+    {
+        timeBeforeDeathStore = timeBeforeDeath;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timeBeforeDeath -= Time.deltaTime;
+
+        if(timeBeforeDeath <= 0 && playerIsTouching == false)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
