b08f88229b0b4f7c867e6bcf5fd7e6762201bb0f
SJCRPV <sjcrpv.games@hotmail.com>
2017-04-22 04:57:57 +0100

    First Commit Game idea is mostly set Learning how to deform meshes to create terraforming powers

---
+++ Ludum-Dare-38/Assets/MeshDeformInput.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshDeformInput : MonoBehaviour {
+
+    [SerializeField]
+    private float force = 5f;
+    [SerializeField]
+    private float forceOffset = 0.01f;
+
+    private void handleInput(int buttonPressed)
+    {
+        if(buttonPressed == 1)
+        {
+            force *= -1;
+        }
+
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(inputRay, out hit))
+        {
+            MeshDeform deformer = hit.collider.GetComponent<MeshDeform>();
+            if(deformer)
+            {
+                Vector3 point = hit.point;
+                point += hit.normal * forceOffset;
+                deformer.addDeformingForce(point, force);
+            }
+        }
+
+        if(buttonPressed == 1)
+        {
+            force *= -1;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            handleInput(0);
+        }
+        else if(Input.GetMouseButton(1))
+        {
+            handleInput(1);
+        }
+	}
+}
